127a84b03855841eb375512437b97f30ba239b11
Josh Thompson <jpthompson93@gmail.com>
2019-11-03 09:45:00 -0800

    completed build.sh script

---
+++ build.sh
@@ -0,0 +1,3 @@
+cat templates/top.html content/index.html templates/bottom.html > docs/index.html
+cat templates/top.html content/blog.html templates/bottom.html > docs/blog.html
+cat templates/top.html content/projects.html templates/bottom.html > docs/projects.html
