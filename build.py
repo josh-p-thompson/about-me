d4bbaf7cb8c3af89c2d351a3746907e6795c35a8
Josh Thompson <jpthompson93@gmail.com>
2019-11-04 18:20:03 -0800

    improved template

--- build.py
+++ build.py
@@ -9,30 +9,16 @@ blog_content = open('content/blog.html').read()
 projects_content = open('content/projects.html').read()
 
 # function to generate pages
-def generate_page(name, content, title): 
+def generate_page(name, content, title, about_btn="", projects_btn="", blog_btn=""): 
     full_page = template.safe_substitute(
         title=title,
-        body_content=content, 
+        right_block_content=content, 
+        about_btn=about_btn, 
+        projects_btn=projects_btn, 
+        blog_btn=blog_btn
     )
     open('docs/' + name + '.html', 'w+').write(full_page)
 
-generate_page(name='index', content=index_content, title='About Me')
-generate_page(name='blog', content=blog_content, title='Blog')
-generate_page(name='projects', content=projects_content, title='Projects')
-
-"""
-bonus_template_text = open('bonus_challenge_template.html').read()
-template = Template(bonus_template_text)
-
-# Use "format" feature of Python strings to insert data where needed for the
-# index page
-index_content = open('middle_index.html').read()
-index_html = template.safe_substitute(
-    title="About Me",
-    index_class="active",
-    content=index_content,
-)
-open('index.html', 'w+').write(index_html)
-
-insert: ${index_class}
-"""
+generate_page(name='index', content=index_content, title='About Me', about_btn='active')
+generate_page(name='blog', content=blog_content, title='Blog', blog_btn='active')
+generate_page(name='projects', content=projects_content, title='Projects', projects_btn='active')
