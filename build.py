e7f3cd6baa62f78300a8d68423304c591551e0c2
Josh Thompson <jpthompson93@gmail.com>
2019-11-07 23:27:45 -0800

    moved all code to functions

--- build.py
+++ build.py
@@ -1,15 +1,8 @@
 from string import Template
-
-# import template
-template = Template(open('templates/template.html').read())
-
-# import content
-index_content = open('content/index.html').read()
-blog_content = open('content/blog.html').read()
-projects_content = open('content/projects.html').read()
-
+ 
 # function to generate pages
 def generate_page(name, content, title, about_btn="", projects_btn="", blog_btn=""): 
+    template = Template(open('templates/template.html').read())
     full_page = template.safe_substitute(
         title=title,
         right_block_content=content, 
@@ -19,6 +12,16 @@ def generate_page(name, content, title, about_btn="", projects_btn="", blog_btn=
     )
     open('docs/' + name + '.html', 'w+').write(full_page)
 
-generate_page(name='index', content=index_content, title='About Me', about_btn='active')
-generate_page(name='blog', content=blog_content, title='Blog', blog_btn='active')
-generate_page(name='projects', content=projects_content, title='Projects', projects_btn='active')
+def main(): 
+    # import content
+    index_content = open('content/index.html').read()
+    blog_content = open('content/blog.html').read()
+    projects_content = open('content/projects.html').read()
+
+    # create pages
+    generate_page(name='index', content=index_content, title='About Me', about_btn='active')
+    generate_page(name='blog', content=blog_content, title='Blog', blog_btn='active')
+    generate_page(name='projects', content=projects_content, title='Projects', projects_btn='active')
+
+if __name__ == "__main__": 
+    main()
